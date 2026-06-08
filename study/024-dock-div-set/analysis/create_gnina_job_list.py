79218bc4f13664abd3ee43c7e237a194519749fb
IanHP <129088911+IanHP@users.noreply.github.com>
2026-06-08 15:48:40 -0400

    Feature: fix 024 create gnina list

--- study/024-dock-div-set/analysis/create_gnina_job_list.py
+++ study/024-dock-div-set/analysis/create_gnina_job_list.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 DIR_SCRIPT: Path = Path(__file__).parent.resolve()
-DIR_STUDY: Path = Path(__file__ ) / ".." / "..".parent.resolve()
+DIR_STUDY: Path = Path(DIR_SCRIPT  / ".." / ".." / "..").resolve()
 
 def main(lig_inp_dir: Path, box_dirs: Path, pdb_dir: Path, output_dir: Path):
     """Will take in (1) ligands to dock (2) boxes to dock in (3) pdb to dock to
