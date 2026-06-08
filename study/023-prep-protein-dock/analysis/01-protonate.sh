413b5016fde0e1bfb853de12ada9c76b6fc6fc8d
ianhp <IRH24@pitt.edu>
2026-06-08 12:07:59 -0400

    Feature: added in reduce to pixi env

--- study/023-prep-protein-dock/analysis/01-protonate.sh
+++ study/023-prep-protein-dock/analysis/01-protonate.sh
@@ -14,6 +14,6 @@ exec > >(tee $FAPC_VS_LOG_FILE_PATH) 2>&1
 
 pdb_name="$(basename $pdb_inp)"
 echo "Protonating $pdb_name..."
-reduce -FLIP &pdb_inp > $pdb_op
+pixi run reduce -FLIP "&pdb_inp" > "$pdb_op"
 echo "Done!"
 
