561e7f51ad911aa97526ad2bcf013e00daa60281
IanHP <129088911+IanHP@users.noreply.github.com>
2026-06-10 11:54:53 -0400

    Feature: safety checks on concat sdf

--- study/024-dock-div-set/analysis/concat_sdf_files.py
+++ study/024-dock-div-set/analysis/concat_sdf_files.py
@@ -21,10 +21,14 @@ def main(lig_inp_dir: Path, lig_op_dir: Path, n_sdf: int):
     lig_list: list[Path] = [item for item in lig_inp_dir.iterdir() if item.is_file()]
     lig_num: float = len(lig_list)
 
+    # not enough sdfs
+    if lig_num < n_sdf:
+        raise Exception("Not enough SDFs, decrease n_sdf")
+
     # determine count in each SDF
     count_per_sdf: int = math.ceil(lig_num / n_sdf)
 
-    min: int = -1
+    min: int = 0
     max: int = count_per_sdf
     
     # for each new file
@@ -33,20 +37,23 @@ def main(lig_inp_dir: Path, lig_op_dir: Path, n_sdf: int):
         # go through correct range and get all files
         if max >= len(lig_list):
             max = len(lig_list) - 1
+        
+        if min >= len(lig_list):
+            raise Exception("Somehow min messed up")
 
         towrite: str = ""
         print(f"FILE NUM: {file_num}")
-        for file_ind in range(min+1, max+1):
+        for file_ind in range(min, max+1):
             #with open(lig_list[file_ind], "r") as f:
                 #towrite = towrite + f.read() + "\n"
             print(f"  {file_ind}")
 
-        min = max
+        min = max + 1
         max = max + count_per_sdf
 
         #with open(file_path, "w") as f:
             #f.write(towrite)
-
+    print(f"{len(lig_list)}")
 
 
 
