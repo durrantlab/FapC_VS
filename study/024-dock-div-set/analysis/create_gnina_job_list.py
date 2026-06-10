8fe4974883a2327d1059b3aa96a1cc59ce9d0cd1
IanHP <129088911+IanHP@users.noreply.github.com>
2026-06-10 10:49:02 -0400

    Fix: add new line to slurm creator

--- study/024-dock-div-set/analysis/create_gnina_job_list.py
+++ study/024-dock-div-set/analysis/create_gnina_job_list.py
@@ -41,7 +41,7 @@ def main(lig_inp_dir: Path, box_dirs: Path, pdb_dir: Path, output_dir: Path):
     with open("run_gnina.sh", "w") as f:
         min = -1
         for max in range(0, len(gnina_inputs)-1, 499)[1:]:
-            f.write(f"sbatch --array={min+1}-{max} --export=ALL dock.slurm")
+            f.write(f"sbatch --array={min+1}-{max} --export=ALL dock.slurm\n")
             min = max
         if min != len(gnina_inputs)-1:
             f.write(f"sbatch --array={min}-{len(gnina_inputs)-1} --export=ALL dock.slurm")
