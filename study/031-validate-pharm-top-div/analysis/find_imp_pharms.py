e6d2211a3011c83dcf83afca2b7d01e40fb74fd7
IanHP <129088911+IanHP@users.noreply.github.com>
2026-06-18 11:19:37 -0400

    Fix: fixed issues with indexing because of H removal

--- study/031-validate-pharm-top-div/analysis/find_imp_pharms.py
+++ study/031-validate-pharm-top-div/analysis/find_imp_pharms.py
@@ -54,12 +54,15 @@ def main(interaction_csv_dir: Path, docked_SDFs: Path,
     for region_name, mol_list in pharm_dict.items():
         # read in the region
         region_file: Path = (docked_SDFs / f"{region_name}_concat.sdf").resolve()
-        supplier = Chem.SDMolSupplier(str(region_file))
+        supplier = Chem.SDMolSupplier(str(region_file), sanitize=True, removeHs=False,
+                                      strictParsing=True)
         new_pharm: list[dict] = []
         pharms_enable_list: list[list[bool]] = []
         for mol_index, mol_pharm in enumerate(mol_list):
             # extract molecule
             mol = supplier[mol_index]
+            if mol_index == 9:
+                print(f"{Chem.MolToSmiles(mol)}")
             # determine if pharmacophore is valid
             reg_res_list: list[str] = residues[region_name]
             reg_inter_type: list[str] = inter_type[region_name]
@@ -172,11 +175,11 @@ def if_inside_pharm(dist, inter_type) -> bool:
 
 if __name__ == "__main__":
     # inputs
-    interaction_csv_dir: Path = (DIR_STUDY / "027-key-top-div-inter" / "data")
-    protein_dir: Path = (DIR_STUDY / "023-prep-protein-dock" / "data" / "9nqd_protonated.pdb")
-    docked_SDFs: Path = (DIR_STUDY / "025-filter-gnina-op" / "data" / "best_drugs")
-    pharm_list: Path = (DIR_STUDY / "028-pharms-top-div-set" / "data")
-    op_dir: Path = (DIR_SCRIPT / ".." / "data")
+    interaction_csv_dir: Path = (DIR_STUDY / "027-key-top-div-inter" / "data").resolve()
+    protein_dir: Path = (DIR_STUDY / "023-prep-protein-dock" / "data" / "9nqd_protonated.pdb").resolve()
+    docked_SDFs: Path = (DIR_STUDY / "025-filter-gnina-op" / "data" / "best_drugs").resolve()
+    pharm_list: Path = (DIR_STUDY / "028-pharms-top-div-set" / "data").resolve()
+    op_dir: Path = (DIR_SCRIPT / ".." / "data").resolve()
 
     main(interaction_csv_dir, docked_SDFs, pharm_list, op_dir)
 
