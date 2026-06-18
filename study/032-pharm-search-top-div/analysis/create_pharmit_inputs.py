a8e64451b99bab82eafe30e2de6c147079e6c76b
IanHP <129088911+IanHP@users.noreply.github.com>
2026-06-18 15:46:18 -0400

    Feature: script to create pharmit inputs automatically

--- study/032-pharm-search-top-div/analysis/create_pharmit_inputs.py
+++ study/032-pharm-search-top-div/analysis/create_pharmit_inputs.py
@@ -6,13 +6,11 @@ DIR_SCRIPT: Path = Path(__file__).parent.resolve()
 DIR_STUDY: Path = Path(DIR_SCRIPT  / ".." / "..").resolve()
 
 sys.path.insert(0, str((DIR_STUDY / "031-validate-pharm-top-div").resolve()))
-from analysis.lib import load_concatenated_json
 
 
-def main(disabled_pharmit_dir: Path, pharmit_output_dir: Path, min_pharm: int = 3):
-    """Will take in pharmits with disabled pharms and create inputs
-    for pharmit with variety of different pharmacophores enabled 
-    / disabled
+def main(disabled_pharmit_dir: Path, pharmit_output_dir: Path, min_pharm: int = 2000):
+    """Will take in pharmits input files and create a slurm to run pharmit with
+    them
 
     Args:
         disabled_pharmit_dir (Path): where the inputs with disabled pharms based
@@ -22,25 +20,45 @@ def main(disabled_pharmit_dir: Path, pharmit_output_dir: Path, min_pharm: int = 
     """
     
     # read in disabled pharmits
-    pharm_list: list[Path] = [item for item in disabled_pharmit_dir.iterdir() if item.is_file() and item.suffix == ".json"]
+    pharmit_inp_region_dirs: list[Path] = [item for item in disabled_pharmit_dir.iterdir() if item.is_dir() and item.name.startswith("region")]
     regions: list[str] = []
-    pharm_dict: dict[str, list[dict]] = {}
-    for pharm_json in pharm_list:
-        region: str = "_".join(pharm_json.name.split(".")[0].split("_")[0:2])
+    pharmit_input_files = []
+    for pharmit_inp_dir in pharmit_inp_region_dirs:
+        region: str = pharmit_inp_dir.name
         regions.append(region)
-    
-    
+        pharmit_input_files.extend([item for item in pharmit_inp_dir.iterdir() if item.is_file() and item.suffix == ".json"])
 
+    # create the pharmit inputs
+    pharmit_inputs: list[str] = []
+    for pharmit_json in pharmit_input_files:
+        output_name: str = pharmit_json.name.split(".")[0]
+        output_sdf: Path = (pharmit_output_dir / pharmit_json.parent.name / f"{output_name}.sdf")
+        output_txt: Path = (pharmit_output_dir / pharmit_json.parent.name / f"{output_name}.txt")
+        if not output_sdf.parent.is_dir():
+            output_sdf.parent.mkdir(parents=True, exist_ok=True)
+        pharmit_inputs.append(f"-in {pharmit_json} -out {output_sdf} {output_txt} -max-hit {min_pharm}")
 
+    # write the job_list
+    job_list_file: Path = (DIR_SCRIPT / "job_list.txt").resolve()
+    with open(job_list_file, "w") as f:
+        for ind, pharmit_input in enumerate(pharmit_inputs):
+            if ind:
+                f.write("\n")
+            f.write(pharmit_input)
+    
+    # create batch script to run slurm
+    batch_script_file: Path = (DIR_SCRIPT / "run_pharmit_search.sh").resolve()
+    with open(batch_script_file, "w") as f:
+        f.write(f"sbatch --array=0-{len(pharmit_input_files)-1} --export=ALL pharm_search.slurm\n")
 
 
 
 
 if __name__ == "__main__":
     # inputs
-    disabled_pharmit_dir: Path = (DIR_SCRIPT / "031-validate-pharm-top-div" / "disabled_pharms_input").resolve()
-    pharmit_output_dir: Path = (DIR_STUDY / ".." / "data" / "pharmit_inputs").resolve()
+    disabled_pharmit_dir: Path = (DIR_STUDY / "031-validate-pharm-top-div" / "data").resolve()
+    pharmit_output_dir: Path = (DIR_SCRIPT / ".." / "data" / "search_output").resolve()
     
-    main(disabled_pharmit_dir, pharmit_output_dir)
+    main(disabled_pharmit_dir, pharmit_output_dir, 2000)
 
 
---
+++ study/032-pharm-search-top-div/analysis/run_pharmit_search.sh
@@ -0,0 +1,1 @@
+sbatch --array=0-29 --export=ALL pharm_search.slurm
