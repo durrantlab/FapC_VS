580897013271492a4c18c00be1628ed9fded41ad
sodium <129088911+IanHP@users.noreply.github.com>
2026-06-21 11:11:48 -0400

    Feature: make dbsetup more mem efficient

--- study/032-create-pharm-db/analysis/setup_dbcreate.py
+++ study/032-create-pharm-db/analysis/setup_dbcreate.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os, tempfile
 
 DIR_SCRIPT: Path = Path(__file__).parent.resolve()
 DIR_STUDY: Path = Path(DIR_SCRIPT  / ".." / "..").resolve()
@@ -30,9 +30,24 @@ def main(sdf_db_path: Path, db_op_path: Path):
             f.write(f" -in {sdf_file}")
 
 
-
-
 def fix_names(sdf_file: Path):
+    index = 0
+    with open(sdf_file) as src, \
+        tempfile.NamedTemporaryFile("w", delete=False, dir=sdf_file.parent.resolve(), newline="") as tmp:
+        tmpname = tmp.name
+        
+        for line in src:                       
+            if line.startswith("  Mrv"):
+                tmp.write(f"  {'_'.join(line.strip().split(' '))}_i{index}\n")
+                index = index+1
+            else:
+                tmp.write(line)
+        
+    os.replace(tmpname, sdf_file)            
+
+
+
+def fix_names_arch(sdf_file: Path):
     # read in file
     with open(sdf_file, "r") as f:
         mole_list = [item.split("\n") for item in f.read().split("\n$$$$\n")]
@@ -51,10 +66,10 @@ def fix_names(sdf_file: Path):
 
 if __name__ == "__main__":
     # inputs
-    sdf_db_path: Path = Path("/ihome/jdurrant/irh24/Projects/molport_cmpds").resolve()
-    #sdf_test_path: Path = Path("F:\\FapC_VS\\study\\032-create-pharm-db\\data\\").resolve()
+    #sdf_db_path: Path = Path("/ihome/jdurrant/irh24/Projects/molport_cmpds").resolve()
+    sdf_test_path: Path = Path("F:\\FapC_VS\\study\\032-create-pharm-db\\data\\").resolve()
     db_op_path: Path = (DIR_STUDY / "data").resolve()
 
-    main(sdf_db_path, db_op_path)
+    main(sdf_test_path, db_op_path)
 
 
