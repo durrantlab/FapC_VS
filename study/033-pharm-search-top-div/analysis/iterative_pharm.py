4d00642f07cedbcd39b995143089759ac79a3035
IanHP <129088911+IanHP@users.noreply.github.com>
2026-06-29 15:25:44 -0400

    Test: final test1 run

--- study/033-pharm-search-top-div/analysis/iterative_pharm.py
+++ study/033-pharm-search-top-div/analysis/iterative_pharm.py
@@ -174,7 +174,7 @@ def run_pharmit(pharm_file: Path, pharm_db_dir: Path, pharmit_output_dir: Path,
             mol_sdf: str = [item.strip() for item in f.read().strip().split("$$$$") if item.strip().startswith(mol_data[2])][0]
             all_mol_sdfs.append(mol_sdf)
     with open(final_sdf, "w") as f:
-        f.write("\n$$$$\n".join(all_mol_sdfs) + "\n$$$$")
+        f.write("\n\n$$$$\n".join(all_mol_sdfs) + "\n$$$$")
     shutil.rmtree(temp_sdf_folder)
     return final_sdf
 
