78e94d75aecc141be16baa8db43fc1551921b6cc
IanHP <129088911+IanHP@users.noreply.github.com>
2026-07-02 11:55:19 -0400

    Fix: idk

--- study/051-dock-pharm-div-set/analysis/create_gnina_job_list.py
+++ study/051-dock-pharm-div-set/analysis/create_gnina_job_list.py
@@ -84,7 +84,7 @@ def clean_up_sdf(sdf_file: Path, op_file: Path) -> str:
     with open(sdf_file, "r") as f:
         mols: list[str] = [item.strip() for item in f.read().strip().split("$$$$")]
     with open(op_file, "w") as f:
-        f.write("\n\n$$$$\n".join(mols)[1:])
+        f.write("\n\n$$$$\n".join(mols[1:]))
     return mols[0]
 
 if __name__ == "__main__":
