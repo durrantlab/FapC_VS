180c514b2dc8dda79a69dd330e0c567729d9f914
IanHP <129088911+IanHP@users.noreply.github.com>
2026-07-02 11:17:25 -0400

    Fix: sigh

--- study/051-dock-pharm-div-set/analysis/create_gnina_job_list.py
+++ study/051-dock-pharm-div-set/analysis/create_gnina_job_list.py
@@ -71,13 +71,23 @@ def main(lig_inp_dir: Path, box_dirs: Path, pdb_dir: Path, cleaned_dir: Path, ou
 
 
 def clean_up_sdf(sdf_file: Path, op_file: Path) -> str:
+    # NOTE: ran incorrectly initially, overwritting original files and adding in new issue,
+    # so temporarily rewrote section to fix that. This is that. Below is script to
+    # use normally
     with open(sdf_file, "r") as f:
         mols: list[str] = [item.strip() for item in f.read().strip().split("$$$$")]
-    with open(sdf_file, "w") as f:
-        f.write("\n\n$$$$\n".join(mols[1:]) + "\n\n$$$$")
+    with open(op_file, "w") as f:
+        f.write("\n\n$$$$\n".join(mols)[:-1])
     return mols[0]
 
-
+"""def clean_up_sdf(sdf_file: Path, op_file: Path) -> str:
+    # NOTE: ran incorrectly initially, overwritting original files and adding in new issue,
+    # so temporarily rewrote section to fix that
+    with open(sdf_file, "r") as f:
+        mols: list[str] = [item.strip() for item in f.read().strip().split("$$$$")]
+    with open(op_file, "w") as f:
+        f.write("\n\n$$$$\n".join(mols)[:-1])
+    return mols[0]"""
 
 if __name__ == "__main__":
     # imports
