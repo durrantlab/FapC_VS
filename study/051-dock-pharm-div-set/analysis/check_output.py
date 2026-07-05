ca9df113e221b1f9a628d9118bd99b04cfdc26fd
sodium <129088911+IanHP@users.noreply.github.com>
2026-07-05 00:10:47 -0400

    Test: if checker working

--- study/051-dock-pharm-div-set/analysis/check_output.py
+++ study/051-dock-pharm-div-set/analysis/check_output.py
@@ -15,11 +15,13 @@ def main(gnina_input_dir: Path, gnina_output_dir: Path):
         if op_path.is_file():
             offset = 0
             with open(inp_path) as f:
-                mol_names_inp: list[str] = [item.split("\n")[0] for item in f.read().strip().split("$$$$")[:-1]]
+                mol_names_inp: list[str] = [item.strip().split("\n")[0] for item in f.read().strip().split("$$$$")[:-1]]
             with open(op_path) as f:
-                mol_names_op: list[str] = [item.split("\n")[0] for item in f.read().strip().split("$$$$")[:-1]]
+                mol_names_op: list[str] = [item.strip().split("\n")[0] for item in f.read().strip().split("$$$$")[:-1]]
             for mol_ind in range(len(mol_names_inp)):
                 if(mol_names_inp != mol_names_op):
+                    print(mol_names_inp[0:10])
+                    print(mol_names_op[0:10])
                     offset = offset + 1
             print(f"Offset by {offset}")
         else:
