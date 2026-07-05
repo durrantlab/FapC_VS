2e0bf5544180ae5558af764e6381a7d7fb875386
sodium <129088911+IanHP@users.noreply.github.com>
2026-07-05 00:21:19 -0400

    Feature: lists all missing molecules in output

--- study/051-dock-pharm-div-set/analysis/check_output.py
+++ study/051-dock-pharm-div-set/analysis/check_output.py
@@ -15,16 +15,16 @@ def main(gnina_input_dir: Path, gnina_output_dir: Path):
         if op_path.is_file():
             offset = 0
             with open(inp_path) as f:
-                mol_names_inp: set[str] = set([item.strip().split("\n")[0].split(" ")[0] for item in f.read().strip().split("$$$$")[:-1]])
+                mol_names_inp: list[str] = [item.strip().split("\n")[0].split(" ")[0] for item in f.read().strip().split("$$$$")[:-1]]
+                mol_names_inp_set: set[str] = set(mol_names_inp)
             with open(op_path) as f:
-                mol_names_op: set[str] = set([item.strip().split("\n")[0].split(" ")[0] for item in f.read().strip().split("$$$$")[:-1]])
-            for mol_ind in range(len(mol_names_inp)):
-                if(mol_names_inp != mol_names_op):
-                    offset = offset + 1
-            if(offset > 0):
-                print(f"{str(lower_path)} is missing molecules")
-                print(mol_names_inp)
-                print(mol_names_op)
+                mol_names_op: list[str] = [item.strip().split("\n")[0].split(" ")[0] for item in f.read().strip().split("$$$$")[:-1]]
+                mol_names_op_set: set[str] = set(mol_names_op)
+
+            for mol in mol_names_inp:
+                if not mol in mol_names_op:
+                    print(f"{str(lower_path)} is missing {mol}")
+
         else:
             print(f"{str(lower_path)} was not created")
 
