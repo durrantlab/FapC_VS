7a93f3654aba29be629a231fe9e35f499b6dc382
sodium <129088911+IanHP@users.noreply.github.com>
2026-07-05 00:13:53 -0400

    Feature: better error messages for output check

--- study/051-dock-pharm-div-set/analysis/check_output.py
+++ study/051-dock-pharm-div-set/analysis/check_output.py
@@ -18,14 +18,13 @@ def main(gnina_input_dir: Path, gnina_output_dir: Path):
                 mol_names_inp: set[str] = set([item.strip().split("\n")[0] for item in f.read().strip().split("$$$$")[:-1]])
             with open(op_path) as f:
                 mol_names_op: set[str] = set([item.strip().split("\n")[0] for item in f.read().strip().split("$$$$")[:-1]])
-            
-            
             for mol_ind in range(len(mol_names_inp)):
                 if(mol_names_inp != mol_names_op):
                     offset = offset + 1
-            print(f"Offset by {offset}")
+            if(offset > 0):
+                print(f"{str(lower_path)} is missing molecules")
         else:
-            print(f"File not created")
+            print(f"{str(lower_path)} was not created")
 
 
 
