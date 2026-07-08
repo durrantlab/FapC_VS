96d99b45e53ae93c547e1d52a3b33ca1bd753e80
IanHP <129088911+IanHP@users.noreply.github.com>
2026-07-08 12:29:37 -0400

    Feature: added in 069, takes outputs of experimental docking and ranks them

---
+++ study/069-filter-gnina-op-pharm-exp/analysis/get_top_drugs.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import csv
+import sys
+
+DIR_SCRIPT: Path = Path(__file__).parent.resolve()
+DIR_STUDY: Path = Path(DIR_SCRIPT  / ".." / "..").resolve()
+
+# add the directory of script to path
+sys.path.insert(0, str((DIR_STUDY / "061-filter-gnina-op" / "analysis").resolve()))
+import get_top_drugs
+
+
+
+
+
+if __name__ == "__main__":
+    # inputs
+    docked_dir: Path = Path(DIR_STUDY / "068-dock-pharm-exp-set" / "data" / "docked_compounds")
+    csv_rank_file: Path = Path(DIR_SCRIPT / ".." / "data" / "ranked_docked_mols.csv")
+    best_drugs: Path = Path(DIR_SCRIPT / ".." / "data" / "best_drugs")
+    
+    get_top_drugs.main(docked_dir, csv_rank_file, best_drugs, 100)
+
+
