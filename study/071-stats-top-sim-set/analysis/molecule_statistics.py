76156c03867acb2a3ee68b1462666235bdcbd0df
IanHP <129088911+IanHP@users.noreply.github.com>
2026-07-13 11:22:59 -0400

    Format: fix file pathing for 071

--- study/071-stats-top-sim-set/analysis/molecule_statistics.py
+++ study/071-stats-top-sim-set/analysis/molecule_statistics.py
@@ -7,15 +7,15 @@ FILE_LOG: Path = (DIR_SCRIPT / ".." / "logs" / f"{Path(__file__).name.split('.')
 
 # add the directory of script to path
 sys.path.insert(0, str((DIR_STUDY / "065-stats-top-sim-set" / "analysis").resolve()))
-import get_top_drugs
+import molecule_statistics
 
 
 if __name__ == "__main__":
     # inputs
     sdf_file: Path = (DIR_STUDY / "069-filter-gnina-op-pharm-exp" / "data" / "best_drugs" / "overall_concat.sdf").resolve()
     csv_rank_file: Path = (DIR_STUDY / "069-filter-gnina-op-pharm-exp" / "data" / "best_drugs" / "overall_best.csv").resolve()
-    models_dir: Path = (DIR_SCRIPT / "models")
+    models_dir: Path = (DIR_STUDY / "065-stats-top-sim-set" / "analysis" / "models").resolve()
     csv_file: Path = (DIR_SCRIPT / ".." / "data" / "top_dock_stats.csv").resolve()
-    get_top_drugs.main(sdf_file, csv_rank_file, models_dir, csv_file)
+    molecule_statistics.main(sdf_file, csv_rank_file, models_dir, csv_file)
 
 
