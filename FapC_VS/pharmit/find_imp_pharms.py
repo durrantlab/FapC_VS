e83a6c6e16b2ca706c6fd37ce885d075438f1c23
sodium <129088911+IanHP@users.noreply.github.com>
2026-07-17 15:15:08 -0400

    Format: e02 and e03 updated

--- FapC_VS/pharmit/find_imp_pharms.py
+++ FapC_VS/pharmit/find_imp_pharms.py
@@ -1,6 +1,124 @@
 import csv
 import json
+import logging
 from pathlib import Path
+
+from rdkit import Chem
+
+
+PROLIF_TO_PHARMACOPHORE = {
+    "Hydrophobic": "Hydrophobic",
+    "HBDonor": "HydrogenDonor",
+    "HBAcceptor": "HydrogenAcceptor",
+    "PiStacking": "Aromatic",
+    "Cationic": "PositiveIon",
+    "Anionic": "NegativeIon",
+}
+
+def make_log_dir(FILE_LOG: Path):
+    if not FILE_LOG.parent.is_dir():
+        FILE_LOG.parent.mkdir(parents=True, exist_ok=True)
+
+
+def main(
+    sdf_path: Path,
+    csv_path: Path,
+    pharm_json_path: Path,
+    op_pharm_dir: Path,
+    op_csv_path: Path,
+    FILE_LOG: Path,
+):
+    """ALL IN CONTEXT OF A SINGLE CONCATED SDF FILE. Requires running of pharmit on that
+    and interaction determination script on it.
+
+    Will take in concated SDF, a csv that lists each molecules interactions (in that sdf), and
+    concat json that lists each molecules pharmacophores (in that sdf). The indexing of each molecule
+    should be same in each (1st in sdf = 1st of pharm = 1st in csv)
+
+    With that, will determine which pharmacophores to disable and enable. Stores csv of which pharms are
+    enable / disable for each molecule (index in csv = index in sdf = etc). Also exports all updated
+    pharmacophore lists, though each molecule has a seperate file
+
+    Args:
+        sdf_path: where sdf with molecules is stored
+        csv_path: where the csv of molecular interactions are stored
+        pharm_json_path: where concat json with all pharmacophores are stored
+        op_pharm_dir: where the updated jsons, 1 for each molecule, will be stored
+            Note: no longer concat, each molecule has a seperate file
+            Index of file = index of molecule in sdf
+            Creates directory path if DNE
+        op_csv_path: where the csv holding which pharms are enabled / disabled is stored
+            Index in csv = index of molecule in sdf
+            Creates directory path if DNE
+    """
+    # set up logging
+    make_log_dir(FILE_LOG)
+    logging.basicConfig(
+        filename=FILE_LOG,
+        level=logging.INFO,
+        format="%(asctime)s  %(levelname)-8s  %(message)s",
+    )
+
+    # read in the csv
+    logging.info("Reading in csv...")
+    residues: list[str] = []
+    """The protein residues interacting"""
+    inter_type: list[str] = []
+    """The type of interaction of those protein residues. Index aligns with above"""
+    if_interact: list[list[str | list[int]]] = []
+    """D1: each different molecule D2: each protein res interaction (index aligns above)"""
+    residues, inter_type, if_interact = read_in_csv(csv_path)
+
+    # read in the pharmacophores concat json
+    logging.info("Reading in pharmacophores...")
+    pharm_list: list[dict] = []
+    """List of pharmacophores for each molecule"""
+    pharm_list = load_in_pharm_json(pharm_json_path)
+
+    # modify pharmacophores
+    logging.info("Modifying pharmacophores...")
+    supplier = Chem.SDMolSupplier(
+        str(sdf_path), sanitize=True, removeHs=False, strictParsing=True
+    )
+    new_pharm: list[dict] = []
+    """New pharmacophores JSON for each molecule"""
+    pharms_enable_list: list[list[bool]] = []
+    """If pharms are enabled / disabled for each molecule"""
+    for mol_index, pharm in enumerate(pharm_list):
+        valid_pharms: list[bool] = get_valid_pharms(
+            pharm, supplier[mol_index], residues, inter_type, if_interact[mol_index]
+        )
+        pharms_enable_list.append(valid_pharms)
+        """Boolean list, if a pharmacophore is valid or not"""
+        log_pharms(mol_index, valid_pharms)
+        new_pharm.append(update_pharm(pharm, valid_pharms))
+
+    # write out the new pharmacophores
+    logging.info("Writing pharmacophores...")
+    if not op_pharm_dir.is_dir():
+        op_pharm_dir.mkdir(parents=True, exist_ok=True)
+    for ind, pharm in enumerate(new_pharm):
+        op_pharm_file: Path = op_pharm_dir / f"mol{ind}_input.json"
+        with open(op_pharm_file, "w") as f:
+            json.dump(pharm, f, indent=2)
+
+    # write out csv of disale / enabled pharms
+    logging.info("Writing csv...")
+    if not op_csv_path.parent.is_dir():
+        op_csv_path.parent.mkdir(parents=True, exist_ok=True)
+    with open(op_csv_path, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(pharms_enable_list)
+
+    logging.info(f"Completed\n")
+
+
+def log_pharms(mol_index: int, valid_pharms: list[bool]):
+    bool_count = valid_pharms.count(True)
+    if bool_count > 3:
+        logging.info(f"Mol{mol_index} has {bool_count} pharms")
+    else:
+        logging.warning(f"Mol{mol_index} has {bool_count} pharms. Check manually")
 
 PROLIF_TO_PHARMACOPHORE = {
     "Hydrophobic": "Hydrophobic",
