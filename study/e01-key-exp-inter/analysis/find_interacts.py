13939f2774c2d62baf6868609f7fa1262f3abafc
sodium <129088911+IanHP@users.noreply.github.com>
2026-07-17 15:07:34 -0400

    Fix: e01 fixed implemented

--- study/e01-key-exp-inter/analysis/find_interacts.py
+++ study/e01-key-exp-inter/analysis/find_interacts.py
@@ -1,31 +1,15 @@
-from typing import Any
-
-import sys
-import warnings
-
-from pandas.core.frame import DataFrame
-
-with warnings.catch_warnings(record=True):
-    from pathlib import Path
-
-    import prolif as plf
-    from rdkit import Chem
-
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-warnings.filterwarnings("ignore", category=UserWarning)
-
+from pathlib import Path
 DIR_SCRIPT: Path = Path(__file__).parent.resolve()
 DIR_STUDY: Path = Path(DIR_SCRIPT / ".." / "..").resolve()
 
 # add the directory of script to path
-sys.path.insert(0, str((DIR_STUDY / "027-key-top-div-inter" / "analysis").resolve()))
-import find_interacts
+from FapC_VS.pharmit import find_key_interact
 
 if __name__ == "__main__":
     # inputs
-    exp_ligands_dir: Path = (DIR_STUDY / "026-compare-sets" / "data").resolve()
+    exp_ligands_dir: Path = (DIR_STUDY / "b04-compare-sets" / "data").resolve()
     protein_file: Path = (
-        DIR_STUDY / "023-prep-protein-dock" / "data" / "9nqd_protonated.pdb"
+        DIR_STUDY / "b01-prep-protein-dock" / "data" / "9nqd_protonated.pdb"
     ).resolve()
-    op_dir: Path = (DIR_STUDY / "062-key-exp-inter" / "data").resolve()
-    find_interacts.main(exp_ligands_dir, protein_file, op_dir)
+    op_dir: Path = (DIR_SCRIPT / ".." / "data").resolve()
+    find_key_interact.main(exp_ligands_dir, protein_file, op_dir)
