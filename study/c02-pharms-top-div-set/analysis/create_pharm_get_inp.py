59dfb59512b5c86813e383583805ab28de71809a
sodium <129088911+IanHP@users.noreply.github.com>
2026-07-17 18:19:18 -0400

    Format: move create_pharm_inp to lib, update c02 readme

--- study/c02-pharms-top-div-set/analysis/create_pharm_get_inp.py
+++ study/c02-pharms-top-div-set/analysis/create_pharm_get_inp.py
@@ -1,55 +1,8 @@
 from pathlib import Path
+from FapC_VS.pharmit import create_pharm_inp
 
 DIR_SCRIPT: Path = Path(__file__).parent.resolve()
 DIR_STUDY: Path = Path(DIR_SCRIPT / ".." / "..").resolve()
-
-HEADER: str = """#!/bin/bash
-#SBATCH --job-name=get_pharms                	 		 # Job name
-#SBATCH --cluster=smp				   	 				 # Use cluster with GPU support
-#SBATCH --partition=preempt             				 # Use preempt partion (free)
-#SBATCH --nodes=1                      					 # Number of nodes
-#SBATCH --ntasks=1                     	 				 # Number of tasks
-#SBATCH --cpus-per-task=32             	 				 # Number of CPU cores per task
-#SBATCH --mem=128G                     	 				 # Memory allocation
-#SBATCH --time=12:00:00                    				 # Time limit (D-HH:MM:SS)
-#SBATCH --output=batch.out                          # Output log path
-
-module purge
-module load pixi
-
-"""
-
-
-def main(top_div_set_dir: Path, output_dir: Path):
-    """Will take in all the SDFs and auto generate a slurm script
-    to extract all pharmacophores from them
-
-    Args:
-        top_div_set_dir: location of top molecules for each
-            region
-        output_dir: where the json list of pharmacophores
-            will be output
-    """
-    slurm_str: str = HEADER
-
-    # get all SDFs
-    div_sdf_list: list[Path] = [
-        item
-        for item in top_div_set_dir.iterdir()
-        if item.is_file() and item.suffix == ".sdf"
-    ]
-
-    # create for every SDF
-    for div_sdf in div_sdf_list:
-        out_path: Path = (output_dir / f"{div_sdf.name.split(".")[0]}.json").resolve()
-        line: str = (
-            f"pixi run -e pharmit pharmit pharma -in {str(div_sdf)} -out {str(out_path)}"
-        )
-        slurm_str = slurm_str + "\n" + line
-
-    slurm_path: Path = (DIR_SCRIPT / "get_pharms.slurm").resolve()
-    with open(slurm_path, "w") as f:
-        f.write(slurm_str)
 
 
 if __name__ == "__main__":
@@ -59,4 +12,4 @@ if __name__ == "__main__":
     ).resolve()
     output_dir: Path = (DIR_STUDY / "c02-pharms-top-div-set" / "data").resolve()
 
-    main(top_div_set_dir, output_dir)
+    create_pharm_inp.main(top_div_set_dir, output_dir, DIR_SCRIPT)
