1c4889a5f2f2c18635f0497c1722e82693995acb
IanHP <129088911+IanHP@users.noreply.github.com>
2026-07-20 14:48:22 -0400

    Fix: paths in c04 script

--- study/c04-pharm-search-top-div/analysis/create_batch_job.py
+++ study/c04-pharm-search-top-div/analysis/create_batch_job.py
@@ -14,13 +14,5 @@ if __name__ == "__main__":
     base_pharmit_output_dir: Path = (DIR_SCRIPT / ".." / "data" / "search_output").resolve()
 
     # go through every region and run create batch job
-    disabled_pharmit_dirs: list[Path] = [
-        item
-        for item in base_disabled_pharmit_dir.iterdir()
-        if item.is_dir() and item.name.startswith("region")
-    ]
-    for disabled_pharmit_dir in disabled_pharmit_dirs:
-        region: str = disabled_pharmit_dir.stem
-        pharmit_output_dir = (base_pharmit_output_dir / region).resolve()
-    create_batch_iterative_pharm_job.main(disabled_pharmit_dir, pharmit_output_dir, 
-        DIR_STUDY, 2000)
+    create_batch_iterative_pharm_job.main(base_disabled_pharmit_dir, 
+        base_pharmit_output_dir, DIR_STUDY, 2000)
